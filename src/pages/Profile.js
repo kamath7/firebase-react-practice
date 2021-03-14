e5dd02352fc368e0fb192d35e184368d68161998
kamath7 <adithyakamath96@gmail.com>
2021-03-15 02:18:25 +0530

    Initial setup. Authentication setup

---
+++ src/pages/Profile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSession } from "../firebase/UsersProvider";
+function Profile() {
+  const { user } = useSession();
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <div>
+      <p>Name: {user.displayName}</p>
+      <p>Email: {user.email}</p>
+      <p>ID: {user.uid}</p>
+    </div>
+  );
+}
+
+export default Profile;
